443c4cfa59342bf20f960db8ee48900de9ba6e37
Lyvia COURLA <lyvia_courla@hotmail.com>
2024-01-05 00:36:21 +0100

    bouton filtres legere,avancee

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const contenerGallery = document.querySelector(".gallery");
+const conteneurBtn = document.querySelector(".filtre");
+
 // ---- Récupération des oeuvres à partir de l'api -----
 const getWorks = async () => {
   const response = await fetch("http://localhost:5678/api/works");
@@ -5,7 +8,7 @@ const getWorks = async () => {
   return works;
 };
 
-
+let indexBouton = 0;
 
 // initiaalisation de la page
 document.addEventListener("DOMContentLoaded", async () => {
@@ -14,52 +17,67 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   // affichage des works
   createGallery(works);
+  // console.log(works);
 
   // recuperer les categories
   const categories = await getCategories();
-  //   console.log(categories);
+  // console.log(categories);
 
-  let indexBouton = 0;
-  const conteneurBtn = document.querySelector(".filter");
   //Affichage des boutons avec le 1er en vert
-    for (let i = 0; i < categories.length; i++) {
-        let whiteBtn = document.createElement("div");
-        whiteBtn.classList.add("btn");
-        conteneurBtn.appendChild(whiteBtn);
-        // noms des categories qui s'affichent dans les boutons hihihi
-        whiteBtn.innerText = categories[i].name;
-        whiteBtn.setAttribute('id', categories[i].id);
+  for (let i = 0; i < categories.length; i++) {
+    let whiteBtn = document.createElement("div");
+    whiteBtn.classList.add("btn");
+    conteneurBtn.appendChild(whiteBtn);
+    // noms des categories qui s'affichent dans les boutons hihihi
+    whiteBtn.innerText = categories[i].name;
+    whiteBtn.setAttribute("id", categories[i].id);
 
-        // creer un evenlistener au clique pour chak btn
-            // inserer le filtrage des boutons
-            // changer la couleur des btn en vert puis blanc
+    /* A faire 
+       creer un evenlistener au clique pour chak btn
+       inserer le filtrage des boutons
+       changer la couleur des btn en vert puis blanc*/
 
-    };
-
-    console.log(conteneurBtn);
-
-    conteneurBtn.forEach((bouton) => {
-        bouton.addEventListener("click", () => {
-          if(bouton.classList.contains("selected")) {
-            return;
-          } else {
-            bouton.classList.add("selected");
-          }
-          
-        });
-        nextBtnSelected();
+    // Eventlistener et changement de couleur
+    whiteBtn[i].addEventListener("click", () => {
+      const ensBtns = document.querySelectorAll(".btn");
+      ensBtns.forEach((whiteBtn) => {
+        whiteBtn.classList.remove("selected");
+      });
+      whiteBtn.classList.add("selected");
     });
 
-    // bouton.addEventListener('click', function () {
-    //     const conteneurBtn = document.querySelectorAll('.btn');
-    //     conteneurBtn.forEach( ( bouton ) => {
-    //         bouton.classList.remove("selected");
-    //     });
-    //     bouton.classList.add("selected");
+    // const ensBtns = document.querySelectorAll(".btn");
+    // for (let i = 0; i < ensBtns.length; i++) {
+    // ensBtns[i].addEventListener("click", () => {
+    if (i !== 0) {
+      allFilter = works.filter((bouton) => bouton.categoryId == i);
+      createGallery(allFilter);
+      console.log(allFilter);
+    } else {
+      createGallery(works);
+    }
+    //   });
+    // }
+
+    // const btnFiltered = categories.filter((bouton, index) => {
+    //   if (whiteBtn == bouton.categories[i]) {
+    //     return bouton;
+    //   }
+    //   console.log(btnFiltered);
     // });
- 
-    
+  }
+  // console.log(whiteBtn);
 });
+
+//     nextBtnSelected();
+
+//     document.querySelector(".btn").addEventListener("click", () => {
+//         const objets = works.filter((event) => {
+//           return event.category.id === 1;
+//         });
+//         createGallery(objets);
+//       });
+// });
 
 // // ----- Récuperation des catégories à partir de l'api -----
 const getCategories = async () => {
@@ -69,10 +87,8 @@ const getCategories = async () => {
 };
 
 // ----- Récupération de la gallery -----
-const createGallery = (works, category) => {
-    console.log(works)
+const createGallery = (works) => {
   works.forEach((work) => {
-    const contenerGallery = document.querySelector(".gallery");
     const figures = document.createElement("figure");
     contenerGallery.appendChild(figures);
 
@@ -90,34 +106,15 @@ const createGallery = (works, category) => {
   });
 };
 
-// const conteneurBtn = document.querySelector(".filter");
-// conteneurBtn.forEach((bouton) => {
-//     bouton.addEventListener("click", () => {
-//       if(bouton.classList.contains("selected")) {
-//         return;
-//       } else {
-//         bouton.classList.add("selected");
-//       }
-//     });
+// const ensBtns = document.querySelectorAll(".btn");
+// for (let i = 0; i < ensBtns.length; i++) {
+//   ensBtns[i].addEventListener("click", () => {
+//     if (i !== 0) {
+//       allFilter = works.filter((bouton) => bouton.categoryId == i);
+//       createGallery(allFilter);
+//       console.log(allFilter);
+//     } else {
+//       createGallery(works);
+//     }
 //   });
-// bouton.addEventListener('click', function () {
-//     const conteneurBtn = document.querySelectorAll('.btn');
-//     conteneurBtn.forEach( ( bouton ) => {
-//         bouton.classList.remove("selected");
-//     });
-//     bouton.classList.add("selected");
-
-// })
-
-      // ----- Changement des boutons 
-      function nextBtnSelected() {
-        let indexBouton = 0;
-        let totalBtn = document.querySelectorAll(".btn");
-            for (let i = 0; i < totalBtn.length; i++){
-             const btn = totalBtn[i];
-                if (i=== indexBouton){
-                btn.classList.add("selected");
-                } else {
-                    btn.classList.remove("selected");
-                }
-        }};
+// }
